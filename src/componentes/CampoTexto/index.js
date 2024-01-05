0affd7607f1a1678451f2690d5be9619bf111bdc
ClebersonEduardo <clebersoneduardo112@gmail.com>
2024-01-04 23:28:45 -0300

    utilizando usestate para armazenar valores do formulario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,18 +3,15 @@ import './CampoTexto.css'
 const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`
-
-    let valor = ''
     
     const aoDigitado = (evento) => {
-        valor = evento.target.value
-        console.log(valor)
+        props.aoAlterado(evento.target.value)
     }
 
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}></input>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}></input>
         </div>
     )
 }
